9766fa2ea4a66d93824d1f72a37cdd52fc714b63
Joyboy5629 <akremhaddad125@gmail.com>
2024-10-14 11:37:19 +0100

    fixing bugs

--- Exercice2.py
+++ Exercice2.py
@@ -17,3 +17,18 @@ print(f"Perimetre du rectangle: {rect1.calculer_perimetre()}")
 rect1 = Rectangle(7, 4)
 print(f"Surface du deuxieme rectangle: {rect1.calculer_surface()}")
 print(f"Perimetre du deuxieme rectangle: {rect1.calculer_perimetre()}")
+
+
+class Voiture:
+    def __init__(self, Marque, Couleur):
+        self.Marque = Marque
+        self.Couleur = Couleur
+
+    def demarer(self):
+        return "la voiture " + self.Couleur + " de marque " + self.Marque + " demare"
+
+
+voiture1 = Voiture("Toyota", "Rouge")
+voiture2 = Voiture("Honda", "Bleu")
+print(voiture1.demarer())
+print(voiture2.demarer())
